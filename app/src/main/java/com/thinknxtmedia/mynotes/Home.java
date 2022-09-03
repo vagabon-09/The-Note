05efbebdfcaf747945a836bf9147efb7f5e57e0f
vagabon-09 <rajeshbhadra62@gmail.com>
2022-09-03 15:10:49 +0530

    Drawer toggle added

--- app/src/main/java/com/thinknxtmedia/mynotes/Home.java
+++ app/src/main/java/com/thinknxtmedia/mynotes/Home.java
@@ -1,14 +1,35 @@
 package com.thinknxtmedia.mynotes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.thinknxtmedia.mynotes.databinding.ActivityHomeBinding;
 
 public class Home extends AppCompatActivity {
+    ActivityHomeBinding bind;
+    Toolbar toolbar;
+    com.thinknxtmedia.mynotes.Navigation.Toolbar tBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        bind = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(bind.getRoot());
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //Creating object of toolbar
+        tBar = new com.thinknxtmedia.mynotes.Navigation.Toolbar();
+        //Finding view
+        getView();
+        //Supporting toolbar
+        setSupportActionBar(toolbar);
+        tBar.ActionToolBar(this, bind.drawerId, toolbar);
+
+    }
+
+    private void getView() {
+        toolbar = findViewById(R.id.toolbar);
     }
 }
---
+++ app/src/main/java/com/thinknxtmedia/mynotes/Navigation/Toolbar.java
@@ -0,0 +1,16 @@
+package com.thinknxtmedia.mynotes.Navigation;
+
+import android.app.Activity;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.thinknxtmedia.mynotes.R;
+
+public class Toolbar {
+    public void ActionToolBar(Activity activity, DrawerLayout drawerLayout, androidx.appcompat.widget.Toolbar toolbar){
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(activity,drawerLayout,toolbar, R.string.open_drawer,R.string.close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+}
