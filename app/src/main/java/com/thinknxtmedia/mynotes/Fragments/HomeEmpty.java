4224143ba383ffa80f7a716141209d1b95686729
vagabon-09 <rajeshbhadra62@gmail.com>
2022-09-05 11:22:10 +0530

    Returnable activities

--- app/src/main/java/com/thinknxtmedia/mynotes/Fragments/HomeEmpty.java
+++ app/src/main/java/com/thinknxtmedia/mynotes/Fragments/HomeEmpty.java
@@ -1,27 +1,24 @@
 package com.thinknxtmedia.mynotes.Fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.thinknxtmedia.mynotes.R;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeEmpty#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.thinknxtmedia.mynotes.R;
+import com.thinknxtmedia.mynotes.ReplaceFreagment.ReplaceFreagment;
+
 public class HomeEmpty extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private static CardView cardBtn;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -30,14 +27,6 @@ public class HomeEmpty extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeEmpty.
-     */
     // TODO: Rename and change types and number of parameters
     public static HomeEmpty newInstance(String param1, String param2) {
         HomeEmpty fragment = new HomeEmpty();
@@ -60,7 +49,14 @@ public class HomeEmpty extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ReplaceFreagment replaceFreagment = new ReplaceFreagment();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_empty, container, false);
+        View v = inflater.inflate(R.layout.fragment_home_empty, container, false);
+        cardBtn = v.findViewById(R.id.addNote_Btn);
+        cardBtn.setOnClickListener(view -> {
+            FragmentManager fm = getParentFragmentManager();
+            replaceFreagment.setItemClickable(R.id.main_container_id, new MakeNote(), fm, "yes");
+        });
+        return v;
     }
 }
