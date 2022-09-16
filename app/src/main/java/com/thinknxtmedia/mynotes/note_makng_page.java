356e5eb22bed20ad500c23d7c9965e1a54c3c9af
vagabon-09 <rajeshbhadra62@gmail.com>
2022-09-17 00:06:19 +0530

    Note taking page is on going..

--- app/src/main/java/com/thinknxtmedia/mynotes/note_makng_page.java
+++ app/src/main/java/com/thinknxtmedia/mynotes/note_makng_page.java
@@ -2,16 +2,24 @@ package com.thinknxtmedia.mynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class note_makng_page extends AppCompatActivity {
+import com.thinknxtmedia.mynotes.databinding.ActivityNoteMakngPageBinding;
 
+public class note_makng_page extends AppCompatActivity {
+    ActivityNoteMakngPageBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_note_makng_page);
+        binding = ActivityNoteMakngPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         //Removing task bar
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+       binding.backBtnId.setOnClickListener(view -> {
+           onBackPressed();
+       });
+
     }
 }
