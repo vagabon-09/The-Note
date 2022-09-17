e346ede6d257cd276c4f8498f6433111102f727a
vagabon-09 <rajeshbhadra62@gmail.com>
2022-09-17 10:57:53 +0530

    Note taking page is on going..

--- app/src/main/java/com/thinknxtmedia/mynotes/note_makng_page.java
+++ app/src/main/java/com/thinknxtmedia/mynotes/note_makng_page.java
@@ -5,6 +5,7 @@ import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.thinknxtmedia.mynotes.databinding.ActivityNoteMakngPageBinding;
 
 public class note_makng_page extends AppCompatActivity {
@@ -17,6 +18,13 @@ public class note_makng_page extends AppCompatActivity {
         //Removing task bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         binding.backBtnId.setOnClickListener(view -> onBackPressed());
+        //Setting button and their function
+        binding.colorPanelId.setOnClickListener(view -> bottomSheet());
+    }
 
+    private void bottomSheet() {
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(R.layout.bottom_sheet);
+        bottomSheetDialog.show();
     }
 }
