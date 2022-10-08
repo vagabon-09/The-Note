d0435108dec187f72ec1654e627ae4a813daec35
vagabon-09 <rajeshbhadra62@gmail.com>
2022-10-08 21:53:15 +0530

    Details page making

--- app/src/main/java/com/thinknxtmedia/mynotes/Update_Note.java
+++ app/src/main/java/com/thinknxtmedia/mynotes/Update_Note.java
@@ -6,12 +6,16 @@ import android.text.Html;
 import android.text.Spanned;
 import android.widget.TextView;
 
+import com.thinknxtmedia.mynotes.databinding.ActivityUpdateNoteBinding;
+
 public class Update_Note extends AppCompatActivity {
+    ActivityUpdateNoteBinding binding;
 TextView title;
 TextView note;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityUpdateNoteBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_update_note);
         title = findViewById(R.id.update_NoteTitleId);
         note = findViewById(R.id.Update_NoteNotesId);
@@ -19,6 +23,8 @@ TextView note;
          Spanned note_u = Html.fromHtml(getIntent().getStringExtra("note"));
         title.setText(title_u);
         note.setText(note_u);
+
+
 
 
 
