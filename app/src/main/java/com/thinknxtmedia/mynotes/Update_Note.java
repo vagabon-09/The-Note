4083442d2827a7fcf9bf3e4cb5d44394733822a7
vagabon-09 <rajeshbhadra62@gmail.com>
2022-10-09 00:29:23 +0530

    Making Updating page

--- app/src/main/java/com/thinknxtmedia/mynotes/Update_Note.java
+++ app/src/main/java/com/thinknxtmedia/mynotes/Update_Note.java
@@ -1,28 +1,34 @@
 package com.thinknxtmedia.mynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import com.thinknxtmedia.mynotes.databinding.ActivityUpdateNoteBinding;
 
+import jp.wasabeef.richeditor.RichEditor;
+
 public class Update_Note extends AppCompatActivity {
     ActivityUpdateNoteBinding binding;
-TextView title;
-TextView note;
+EditText title;
+RichEditor note;
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityUpdateNoteBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_update_note);
+        setContentView(binding.getRoot());
         title = findViewById(R.id.update_NoteTitleId);
         note = findViewById(R.id.Update_NoteNotesId);
-         Spanned title_u = Html.fromHtml(getIntent().getStringExtra("title"));
-         Spanned note_u = Html.fromHtml(getIntent().getStringExtra("note"));
+        Spanned title_u = Html.fromHtml(getIntent().getStringExtra("title"));
         title.setText(title_u);
-        note.setText(note_u);
+
+        binding.UpdateNoteNotesId.getSettings().setJavaScriptEnabled(true);
+        binding.UpdateNoteNotesId.loadData(getIntent().getStringExtra("note"), "text/html; charset=utf-8", "UTF-8");
 
 
     }
