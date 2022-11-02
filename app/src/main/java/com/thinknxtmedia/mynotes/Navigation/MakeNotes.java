1a389ec9b27a73de72e92c793abe72feab8be80c
Sand Man <rajeshbhadra62@gmail.com>
2022-11-03 02:29:33 +0530

    code formatting..

--- app/src/main/java/com/thinknxtmedia/mynotes/Navigation/MakeNotes.java
+++ app/src/main/java/com/thinknxtmedia/mynotes/Navigation/MakeNotes.java
@@ -2,10 +2,8 @@ package com.thinknxtmedia.mynotes.Navigation;
 
 import android.app.Activity;
 
-import java.util.zip.Inflater;
-
 public class MakeNotes {
-    public void notes(Activity homeEmpty){
+    public void notes(Activity homeEmpty) {
 
 
     }
